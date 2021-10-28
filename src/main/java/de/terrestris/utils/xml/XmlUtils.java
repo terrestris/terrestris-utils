19c7c813bfba2c3b828f4b55f782cc4c0dea20f6
Andreas Schmitz <schmitz@terrestris.de>
2021-10-28 12:44:47 +0200

    Add github actions

--- src/main/java/de/terrestris/utils/xml/XmlUtils.java
+++ src/main/java/de/terrestris/utils/xml/XmlUtils.java
@@ -28,113 +28,113 @@ import static javax.xml.stream.XMLStreamConstants.END_DOCUMENT;
  */
 public class XmlUtils {
 
-    private XmlUtils() {
-        // prevent instantiation
-    }
+  private XmlUtils() {
+    // prevent instantiation
+  }
 
-    /**
-     * Skip the stream to the next element with the given local name. Precondition can be anything, postcondition will be
-     * START_ELEMENT.
-     *
-     * @param reader    the XML stream reader
-     * @param localName the desired local name
-     * @throws XMLStreamException if anything goes wrong
-     */
-    public static void skipToElement(XMLStreamReader reader, String localName) throws XMLStreamException {
-        while (!(reader.isStartElement() && reader.getLocalName().equals(localName))) {
-            reader.next();
-        }
+  /**
+   * Skip the stream to the next element with the given local name. Precondition can be anything, postcondition will be
+   * START_ELEMENT.
+   *
+   * @param reader    the XML stream reader
+   * @param localName the desired local name
+   * @throws XMLStreamException if anything goes wrong
+   */
+  public static void skipToElement(XMLStreamReader reader, String localName) throws XMLStreamException {
+    while (!(reader.isStartElement() && reader.getLocalName().equals(localName))) {
+      reader.next();
     }
+  }
 
-    /**
-     * Skips the stream to the first occurrence of any of the given local names. Precondition can be anything, postcondition
-     * will be START_ELEMENT.
-     *
-     * @param reader     the XML stream reader
-     * @param localNames the local names
-     * @throws XMLStreamException if anything goes wrong
-     */
-    public static void skipToOneOf(XMLStreamReader reader, String... localNames) throws XMLStreamException {
-        List<String> list = Arrays.asList(localNames);
-        while (!(reader.isStartElement() && list.contains(reader.getLocalName()))) {
-            reader.next();
-            if (!reader.hasNext()) {
-                break;
-            }
-        }
+  /**
+   * Skips the stream to the first occurrence of any of the given local names. Precondition can be anything, postcondition
+   * will be START_ELEMENT.
+   *
+   * @param reader     the XML stream reader
+   * @param localNames the local names
+   * @throws XMLStreamException if anything goes wrong
+   */
+  public static void skipToOneOf(XMLStreamReader reader, String... localNames) throws XMLStreamException {
+    List<String> list = Arrays.asList(localNames);
+    while (!(reader.isStartElement() && list.contains(reader.getLocalName()))) {
+      reader.next();
+      if (!reader.hasNext()) {
+        break;
+      }
     }
+  }
 
-    /**
-     * Writes a simple element with text content.
-     *
-     * @param writer    the XMLStreamWriter
-     * @param namespace the namespace
-     * @param localName the local name
-     * @param content   the text content
-     * @throws XMLStreamException if anything goes wrong
-     */
-    public static void writeSimpleElement(XMLStreamWriter writer, String namespace, String localName, String content)
-        throws XMLStreamException {
-        writer.writeStartElement(namespace, localName);
-        writer.writeCharacters(content);
-        writer.writeEndElement();
-    }
+  /**
+   * Writes a simple element with text content.
+   *
+   * @param writer    the XMLStreamWriter
+   * @param namespace the namespace
+   * @param localName the local name
+   * @param content   the text content
+   * @throws XMLStreamException if anything goes wrong
+   */
+  public static void writeSimpleElement(XMLStreamWriter writer, String namespace, String localName, String content)
+    throws XMLStreamException {
+    writer.writeStartElement(namespace, localName);
+    writer.writeCharacters(content);
+    writer.writeEndElement();
+  }
 
-    /**
-     * Copy a subtree into the given writer. Needs API version 2 in order to use #copyEventFromReader.
-     * The reader must be on a START_ELEMENT event and will be on the corresponding END_ELEMENT event after copying.
-     * Please note that the start element occurring in the subtree will most likely lead to broken output.
-     *
-     * @param reader the reader to read from
-     * @param writer the writer to write to
-     * @throws XMLStreamException if anything goes wrong
-     */
-    public static void copySubtree(XMLStreamReader2 reader, XMLStreamWriter2 writer) throws XMLStreamException {
-        String localName = reader.getLocalName();
-        String ns = reader.getNamespaceURI();
-        while (!(reader.isEndElement() && reader.getLocalName().equals(localName) && reader.getNamespaceURI().equals(ns))) {
-            writer.copyEventFromReader(reader, true);
-            reader.next();
-        }
+  /**
+   * Copy a subtree into the given writer. Needs API version 2 in order to use #copyEventFromReader.
+   * The reader must be on a START_ELEMENT event and will be on the corresponding END_ELEMENT event after copying.
+   * Please note that the start element occurring in the subtree will most likely lead to broken output.
+   *
+   * @param reader the reader to read from
+   * @param writer the writer to write to
+   * @throws XMLStreamException if anything goes wrong
+   */
+  public static void copySubtree(XMLStreamReader2 reader, XMLStreamWriter2 writer) throws XMLStreamException {
+    String localName = reader.getLocalName();
+    String ns = reader.getNamespaceURI();
+    while (!(reader.isEndElement() && reader.getLocalName().equals(localName) && reader.getNamespaceURI().equals(ns))) {
+      writer.copyEventFromReader(reader, true);
+      reader.next();
     }
+  }
 
-    /**
-     * Use the default transformer to serialize a DOM node to string.
-     *
-     * @param node the DOM node to serialize
-     * @return the string with the serialized XML
-     * @throws TransformerException if the transformation failed
-     * @throws IOException          if the transformation failed
-     */
-    public static String domToString(Node node) throws TransformerException, IOException {
-        TransformerFactory factory = TransformerFactory.newDefaultInstance();
-        Transformer transformer = factory.newTransformer();
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
-            transformer.transform(new DOMSource(node), new StreamResult(bos));
-            return bos.toString(UTF_8);
-        }
+  /**
+   * Use the default transformer to serialize a DOM node to string.
+   *
+   * @param node the DOM node to serialize
+   * @return the string with the serialized XML
+   * @throws TransformerException if the transformation failed
+   * @throws IOException          if the transformation failed
+   */
+  public static String domToString(Node node) throws TransformerException, IOException {
+    TransformerFactory factory = TransformerFactory.newDefaultInstance();
+    Transformer transformer = factory.newTransformer();
+    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+    try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+      transformer.transform(new DOMSource(node), new StreamResult(bos));
+      return bos.toString(UTF_8);
     }
+  }
 
-    /**
-     * Move the reader to the next {@link XMLStreamConstants #START_ELEMENT} or {@link XMLStreamConstants #END_ELEMENT}
-     * event.
-     *
-     * @param xmlReader the XML stream reader
-     * @return either the start or end element event type
-     * @throws XMLStreamException     if anything goes wrong
-     * @throws NoSuchElementException if the end of the document is reached
-     */
-    public static int nextElement(XMLStreamReader xmlReader)
-        throws XMLStreamException, NoSuchElementException {
-        xmlReader.next();
-        while (xmlReader.getEventType() != END_DOCUMENT && !xmlReader.isStartElement() && !xmlReader.isEndElement()) {
-            xmlReader.next();
-        }
-        if (xmlReader.getEventType() == END_DOCUMENT) {
-            throw new NoSuchElementException();
-        }
-        return xmlReader.getEventType();
+  /**
+   * Move the reader to the next {@link XMLStreamConstants #START_ELEMENT} or {@link XMLStreamConstants #END_ELEMENT}
+   * event.
+   *
+   * @param xmlReader the XML stream reader
+   * @return either the start or end element event type
+   * @throws XMLStreamException     if anything goes wrong
+   * @throws NoSuchElementException if the end of the document is reached
+   */
+  public static int nextElement(XMLStreamReader xmlReader)
+    throws XMLStreamException, NoSuchElementException {
+    xmlReader.next();
+    while (xmlReader.getEventType() != END_DOCUMENT && !xmlReader.isStartElement() && !xmlReader.isEndElement()) {
+      xmlReader.next();
     }
+    if (xmlReader.getEventType() == END_DOCUMENT) {
+      throw new NoSuchElementException();
+    }
+    return xmlReader.getEventType();
+  }
 
 }
